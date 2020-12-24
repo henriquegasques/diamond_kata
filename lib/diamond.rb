a1e10bd106e60449035547e8e2877ad6b57b5a1d
Henrique Gasques <henriquegasques@gmail.com>
2020-12-24 18:44:16 -0300

    Extract lower level logic to private methods

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,19 +1,33 @@
 class Diamond
+  attr_reader :separator
+
+  def initialize
+    @separator = "_"
+  end
 
   def build(character)
     return "A\n" if character == 'A'
 
-    diamond, underscores = 'B'.upto(character).reduce(["A", 1]) do |(diamond, underscores), letter|
-      new_diamond = diamond + "\n" + letter + ("_" * underscores) + letter
+    diamond_base, separator_quantity = build_diamond_base(character)
+    top_to_middle = centralize(diamond_base, separator_quantity)
 
-      [new_diamond, underscores + 2]
+    _middle_line, *bottom_lines = top_to_middle.reverse.lines
+    top_to_middle + "\n" + bottom_lines.join + "\n"
+  end
+
+  private
+
+  def build_diamond_base(character)
+    'B'.upto(character).reduce(["A", 1]) do |(diamond, separator_quantity), letter|
+      new_diamond = diamond + "\n" + letter + (separator * separator_quantity) + letter
+
+      [new_diamond, separator_quantity + 2]
     end
+  end
 
-    diamond = diamond.each_line.map.with_index do |diamond_line, index|
-      index == diamond.lines.count - 1 ? diamond_line : diamond_line.chomp.center(underscores, "_")
+  def centralize(diamond_base, separator_quantity)
+    diamond_base.lines.map do |diamond_line|
+      diamond_line.chomp.center(separator_quantity, separator)
     end.join("\n")
-
-    _middle, *diamond_bottom = diamond.reverse.lines
-    diamond + "\n" + diamond_bottom.join + "\n"
   end
 end
